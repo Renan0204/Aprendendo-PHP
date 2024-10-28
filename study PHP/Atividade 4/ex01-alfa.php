aa52b6848f45747bd127fe3ecf9e11b790a04915
Renan Oliveira <ro6680476@gmail.com>
2024-10-28 19:45:27 -0300

    Add files via upload

--- study PHP/Atividade 4/ex01-alfa.php
+++ study PHP/Atividade 4/ex01-alfa.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Info</title>
+    <title>PHP</title>
 </head>
 <body>
-    <h1>Dados do Servidor</h1>
     <?php 
-        phpinfo();    
+        print "Hello World!!! \u{1F30E}";
     ?>
 </body>
-</html> 
+</html>
